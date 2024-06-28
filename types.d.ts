41134b7d4211edd55e8e7aeafe1cdca45e8bdbd3
deMGoncalves <cleber.engineer@hotmail.com>
2024-06-28 12:15:56 -0300

    ci: npm publish

--- types.d.ts
+++ types.d.ts
@@ -1,1 +1,11 @@
-export * from "standard/define";
+/**
+ * Função decoradora para definir elementos personalizados.
+ *
+ * @param {string} tagName - O nome do elemento personalizado.
+ * @param {ElementDefinitionOptions} options - As opções para definir o elemento personalizado.
+ * @returns {Function} - Uma função decoradora para definir a classe do elemento personalizado.
+ */
+export declare function define(
+	tagName: string,
+	options?: ElementDefinitionOptions,
+): (constructor: CustomElementConstructor) => void;
