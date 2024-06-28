b0c7e7e4d4168a6bff5434a4de21bb93f8c21599
deMGoncalves <cleber.engineer@hotmail.com>
2024-06-28 12:59:42 -0300

    ci: configuracao dos types.d.ts

--- types.d.ts
+++ types.d.ts
@@ -1,1 +1,11 @@
-/// <reference path="./packages/standard/define/types.d.ts" />
+/**
+ * Função decoradora para definir elementos personalizados.
+ *
+ * @param {string} tagName - O nome do elemento personalizado.
+ * @param {ElementDefinitionOptions} options - As opções para definir o elemento personalizado.
+ * @returns {Function} - Uma função decoradora para definir a classe do elemento personalizado.
+ */
+export declare function define(
+	tagName: string,
+	options?: ElementDefinitionOptions,
+): (constructor: CustomElementConstructor) => void;
