a7ad188e573c4ab45b29202b19582ee2914c3cd8
deMGoncalves <cleber.engineer@hotmail.com>
2024-07-02 21:46:07 -0300

    docs: melhora o jsdoc e tipagem do paint

--- src/paint/types.d.ts
+++ src/paint/types.d.ts
@@ -1,9 +1,23 @@
 /**
  * Decorator utilizado para renderizar o conteúdo do elemento customizado.
  *
- * @param {Function} component - Uma função assíncrona que retorna o conteúdo HTML a ser renderizado.
- * @returns {Function} - Uma função que aplica o decorator a um elemento alvo.
+ * @param {Component} component - Uma função assíncrona que retorna o conteúdo HTML a ser renderizado.
+ * @returns {Decorator} - Uma função que aplica o decorator a um elemento alvo.
  */
-export declare function paint(
-  component: (target: any) => string,
-): (constructor: CustomElementConstructor) => void;
+export declare function paint(component: Component): Decorator;
+
+/**
+ * Função que retorna o conteúdo HTML a ser renderizado.
+ *
+ * @param {any} target - O elemento alvo que será renderizado.
+ * @returns {Promise<string> | string} - O conteúdo HTML a ser renderizado.
+ */
+type Component = (target: any) => Promise<string> | string;
+
+/**
+ * Função decoradora que aplica o decorator a um elemento alvo.
+ *
+ * @param {CustomElementConstructor} constructor - O construtor do elemento customizado.
+ * @returns {void} - Não retorna nenhum valor.
+ */
+type Decorator = (constructor: CustomElementConstructor) => void;
