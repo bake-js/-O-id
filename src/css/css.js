deda9119a3bfe5497685778412cffaa3f69b65eb
deMGoncalves <cleber.engineer@hotmail.com>
2024-07-31 11:46:01 -0300

    refactor(dom): move o css e html para o contexto do dom

--- src/css/css.js
+++
@@ -1,8 +0,0 @@
-const css = (strings, ...values) => {
-  const style = new CSSStyleSheet();
-  const textContent = String.raw({ raw: strings }, ...values);
-  style.replaceSync(textContent);
-  return style;
-};
-
-export default css;
