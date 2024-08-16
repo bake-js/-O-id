9d25a37bc16f5c4e82ad5450e033cf84dd82589a
deMGoncalves <cleber.engineer@hotmail.com>
2024-08-16 11:15:14 -0300

    test: inclusao de novos testes

--- src/define/define.test.ts
+++ src/define/define.test.ts
@@ -6,7 +6,7 @@ describe("define", () => {
     customElements.define = mock(() => {});
   });
 
-  it("Registra o custom element com o name passado na execucao do decorator", () => {
+  it("Registra o custom element com o name passado na execução do decorator", () => {
     @define("element-counter")
     class Element {}
 
@@ -14,6 +14,39 @@ describe("define", () => {
       "element-counter",
       Element,
       undefined,
+    );
+  });
+
+  it("Deve registrar múltiplos custom elements corretamente", () => {
+    @define("element-one")
+    class ElementOne {}
+
+    expect(customElements.define).toHaveBeenCalledWith(
+      "element-one",
+      ElementOne,
+      undefined,
+    );
+
+    @define("element-two")
+    class ElementTwo {}
+
+    expect(customElements.define).toHaveBeenCalledWith(
+      "element-two",
+      ElementTwo,
+      undefined,
+    );
+  });
+
+  it("Deve permitir a configuração de um terceiro parâmetro ao registrar o custom element", () => {
+    const options = { extends: "button" };
+
+    @define("custom-button", options)
+    class CustomButton {}
+
+    expect(customElements.define).toHaveBeenCalledWith(
+      "custom-button",
+      CustomButton,
+      options,
     );
   });
 });
