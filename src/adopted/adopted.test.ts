598eb5eef68be175eb0b79af1a2f486897587fa7
deMGoncalves <cleber.engineer@hotmail.com>
2024-08-16 09:57:52 -0300

    test: inclusao de novos teste e padronizacao do codigo

--- src/adopted/adopted.test.ts
+++ src/adopted/adopted.test.ts
@@ -3,28 +3,32 @@ import adopted from "./adopted";
 
 describe("adopted", () => {
   let element: Element;
-  let lifecycle: string[];
+  let lifeCycle: string[];
 
   class Element {
     adoptedCallback() {
-      lifecycle.push("adoptedCallback");
+      lifeCycle.push("adoptedCallback");
       return this;
     }
 
     @adopted
-    onAdopted() {
-      lifecycle.push("onAdopted");
+    handleAdoption() {
+      lifeCycle.push("handleAdoption");
       return this;
     }
   }
 
   beforeEach(() => {
     element = new Element();
-    lifecycle = [];
+    lifeCycle = [];
   });
 
-  it("Executa o metodo decorado pelo adopted apos o evento adoptedCallback", async () => {
+  it("Deve executar o método decorado pelo @adopted após a chamada de adoptedCallback", async () => {
     await element.adoptedCallback();
-    expect(lifecycle).toEqual(["adoptedCallback", "onAdopted"]);
+    expect(lifeCycle).toEqual(["adoptedCallback", "handleAdoption"]);
+  });
+
+  it("Não deve executar o método decorado sem a chamada de adoptedCallback", () => {
+    expect(lifeCycle).toEqual([]);
   });
 });
