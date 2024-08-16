cfb706eaf99d491f0ab72ab3f2c192f46ecc37de
deMGoncalves <cleber.engineer@hotmail.com>
2024-08-16 10:50:55 -0300

    test: inclusao de novos testes

--- src/connected/connected.test.ts
+++ src/connected/connected.test.ts
@@ -1,7 +1,7 @@
 import { beforeEach, describe, expect, it } from "bun:test";
 import connected from "./connected";
 
-describe("attributeChanged", () => {
+describe("connected", () => {
   let element: Element;
   let lifecycle: string[];
 
@@ -12,8 +12,8 @@ describe("attributeChanged", () => {
     }
 
     @connected
-    onConnected(value, oldValue) {
-      lifecycle.push("onConnected");
+    setup(value, oldValue) {
+      lifecycle.push("setup");
       return this;
     }
   }
@@ -23,8 +23,43 @@ describe("attributeChanged", () => {
     lifecycle = [];
   });
 
-  it("Executa o metodo decorado somente quando o atributo for o mesmo mencionado", async () => {
+  it("Deve executar o método decorado @connected após a chamada de connectedCallback", async () => {
     await element.connectedCallback();
-    expect(lifecycle).toEqual(["connectedCallback", "onConnected"]);
+    expect(lifecycle).toEqual(["connectedCallback", "setup"]);
+  });
+
+  it("Não deve executar o método decorado se connectedCallback não for chamado", () => {
+    expect(lifecycle).toEqual([]);
+  });
+
+  it("Executa o método decorado @connected apenas uma vez quando connectedCallback é chamado múltiplas vezes", async () => {
+    await element.connectedCallback();
+    await element.connectedCallback();
+
+    expect(lifecycle).toEqual([
+      "connectedCallback",
+      "setup",
+      "connectedCallback",
+      "setup",
+    ]);
+  });
+
+  it("Garante que o método decorado @connected seja chamado mesmo quando connectedCallback é chamado após uma alteração no estado", async () => {
+    await element.connectedCallback();
+    lifecycle = []; // Resetando o ciclo de vida para a segunda verificação
+
+    // Simulando uma mudança de estado
+    element.setup("newValue", "oldValue");
+    await element.connectedCallback();
+
+    expect(lifecycle).toEqual(["setup", "connectedCallback", "setup"]);
+  });
+
+  it("Não deve adicionar duplicatas no ciclo de vida ao chamar setup diretamente", async () => {
+    await element.setup("newValue", "oldValue");
+    expect(lifecycle).toEqual(["setup"]);
+
+    await element.connectedCallback();
+    expect(lifecycle).toEqual(["setup", "connectedCallback", "setup"]);
   });
 });
