f7dd497b325917a651409a698e20af36e3f1ee1f
deMGoncalves <cleber.engineer@hotmail.com>
2024-08-16 17:18:32 -0300

    test: casoss de testes do css

---
+++ src/dom/css/css.test.js
@@ -0,0 +1,60 @@
+import { describe, expect, it } from "bun:test";
+import css from "./css";
+
+describe("css", () => {
+  it("Deve criar uma instância de CSSStyleSheet", () => {
+    const [style] = css`body { background-color: black; }`;
+    expect(style).toBeInstanceOf(CSSStyleSheet);
+  });
+
+  it("Deve definir o conteúdo correto da folha de estilo", () => {
+    const [style] = css`
+      body {
+        background-color: black;
+      }
+    `;
+
+    expect(style.cssRules[0].cssText).toBe("body { background-color: black; }");
+  });
+
+  it("Deve substituir corretamente os valores dentro do template literal", () => {
+    const color = "red";
+    const [style] = css`
+      body {
+        background-color: ${color};
+      }
+    `;
+
+    expect(style.cssRules[0].cssText).toBe("body { background-color: red; }");
+  });
+
+  it("Deve lidar com múltiplos valores e regras de CSS", () => {
+    const bgColor = "blue";
+    const fontSize = "16px";
+    const [style] = css`
+      body {
+        background-color: ${bgColor};
+        font-size: ${fontSize};
+      }
+    `;
+
+    expect(style.cssRules[0].cssText).toBe(
+      "body { background-color: blue; font-size: 16px; }",
+    );
+  });
+
+  it("Deve retornar uma folha de estilo com múltiplas regras", () => {
+    const [style] = css`
+      body {
+        background-color: black;
+      }
+      h1 {
+        color: white;
+      }
+    `;
+
+    expect(style.cssRules.length).toBe(2);
+    expect(style.cssRules[0].cssText).toBe("body { background-color: black; }");
+    expect(style.cssRules[1].cssText).toBe("h1 { color: white; }");
+  });
+});
