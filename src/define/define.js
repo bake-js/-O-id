5c20cba465c2695f2e25ef9a6f3d98e8fd64acd4
deMGoncalves <cleber.engineer@hotmail.com>
2024-08-23 22:01:51 -0300

    docs: padroniza o jsdoc do modulo define

--- src/define/define.js
+++ src/define/define.js
@@ -1,11 +1,27 @@
 /**
  * Decorator para definir um Custom Element.
  *
- * Este decorator registra uma classe como um Custom Element com o nome e opções fornecidos.
- *
  * @param {string} name - O nome do Custom Element a ser registrado.
  * @param {ElementDefinitionOptions} [options] - Opções adicionais para a definição do Custom Element.
  * @returns {Function} Um decorator que define o Custom Element.
+ *
+ * @description
+ * O decorator `define` registra uma classe como um Custom Element com o nome e opções fornecidos.
+ * Quando aplicado a uma classe, ele chama `customElements.define` para registrar a classe como um Custom Element
+ * com o nome especificado. As opções adicionais podem ser usadas para fornecer configurações adicionais para a definição
+ * do Custom Element.
+ *
+ * @example
+ * // Exemplo de uso do decorator `define`
+ * import { define } from '@bake-js/-o-id';
+ *
+ * @define('my-element', { extends: 'div' })
+ * class MyElement extends HTMLDivElement {
+ *   constructor() {
+ *     super();
+ *     this.textContent = 'Hello, world!';
+ *   }
+ * }
  */
 const define = (name, options) => (constructor) => {
   // Define o Custom Element com o nome e opções fornecidos.
