2f312752690e154f319779cdc1825b671d4f4a8e
deMGoncalves <cleber.engineer@hotmail.com>
2024-08-23 21:57:45 -0300

    docs: padroniza do jsdoc do modulo attribute changed

--- src/attributeChanged/attributeChanged.js
+++ src/attributeChanged/attributeChanged.js
@@ -5,8 +5,27 @@ import { attributeChangedCallback, observedAttributes } from "../interfaces";
  * Decorator que adiciona lógica ao método `attributeChangedCallback` de um Custom Element.
  * Permite que um método seja executado quando um atributo específico é alterado.
  *
- * @param attributeName - O nome do atributo a ser monitorado.
- * @returns Um decorator que intercepta a chamada do `attributeChangedCallback`.
+ * @param {string} attributeName - O nome do atributo a ser monitorado.
+ * @returns {Function} Um decorator que intercepta a chamada do `attributeChangedCallback`.
+ *
+ * @description
+ * O decorator `attributeChanged` é utilizado para adicionar lógica ao método `attributeChangedCallback`
+ * de um Custom Element. Quando um atributo específico (definido por `attributeName`) é alterado, o método
+ * decorado é chamado com os valores antigo e novo do atributo. O decorator também garante que o atributo
+ * seja adicionado à lista de atributos observados do Custom Element.
+ *
+ * @example
+ * // Exemplo de uso do decorator `attributeChanged`
+ * import { attributeChanged } from '@bake-js/-o-id';
+ *
+ * class MyElement extends HTMLElement {
+ *   @attributeChanged('my-attribute')
+ *   handleAttributeChange(newValue, oldValue) {
+ *     console.log(`Atributo alterado de ${oldValue} para ${newValue}`);
+ *   }
+ * }
+ *
+ * customElements.define('my-element', MyElement);
  */
 const attributeChanged = (attributeName) => (target, propertyKey) => {
   // Atualiza a lista de atributos observados do Custom Element.
