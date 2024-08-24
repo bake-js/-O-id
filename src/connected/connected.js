059c5bce5d8d882b5232bb8f9dd0d610b59fc970
deMGoncalves <cleber.engineer@hotmail.com>
2024-08-23 21:59:18 -0300

    docs: padroniza do jsdoc do modulo connected

--- src/connected/connected.js
+++ src/connected/connected.js
@@ -4,9 +4,27 @@ import { connectedCallback } from "../interfaces";
 /**
  * Decorator que adiciona lógica ao método `connectedCallback` de um Custom Element.
  *
- * @param target - O alvo do decorator, geralmente a classe do Custom Element.
- * @param propertyKey - O nome do método decorado.
- * @returns Um decorator que intercepta a chamada do `connectedCallback`.
+ * @param {Object} target - O alvo do decorator, geralmente a classe do Custom Element.
+ * @param {string} propertyKey - O nome do método decorado.
+ * @returns {Function} Um decorator que intercepta a chamada do `connectedCallback`.
+ *
+ * @description
+ * O decorator `connected` é utilizado para adicionar lógica ao método `connectedCallback` de um Custom Element.
+ * Quando o Custom Element é inserido no DOM, o método decorado é chamado. O decorator configura um interceptor
+ * para que o método decorado seja executado quando o `connectedCallback` do Custom Element é invocado.
+ *
+ * @example
+ * // Exemplo de uso do decorator `connected`
+ * import { connected } from '@bake-js/-o-id';
+ *
+ * class MyElement extends HTMLElement {
+ *   @connected
+ *   handleConnected() {
+ *     console.log('O elemento foi conectado ao DOM.');
+ *   }
+ * }
+ *
+ * customElements.define('my-element', MyElement);
  */
 const connected = (target, propertyKey) => {
   // Cria uma instância do interceptor para o método `connectedCallback`.
