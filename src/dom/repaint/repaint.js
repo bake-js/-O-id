f259ef8b128f4c76f655e616d1a749e30d913639
deMGoncalves <cleber.engineer@hotmail.com>
2024-08-25 17:06:29 -0300

    docs: padroniza o jsdoc do repaint

--- src/dom/repaint/repaint.js
+++ src/dom/repaint/repaint.js
@@ -3,10 +3,26 @@ import { paintCallback } from "../interfaces";
 /**
  * Decorator para chamar o callback de pintura após a execução do método original.
  *
+ * O decorator garante que o callback `paintCallback` seja chamado após a execução do método original,
+ * caso o elemento esteja conectado ao DOM. É útil para garantir que ações de pintura sejam feitas
+ * no momento certo do ciclo de vida do componente.
+ *
  * @param {Object} _target - O alvo do decorator (classe ou protótipo).
  * @param {string} _propertyKey - O nome da propriedade/método decorado.
  * @param {Object} descriptor - O descritor de propriedade/método.
  * @returns {void}
+ *
+ * @example
+ * class MyComponent extends HTMLElement {
+ *   @repaint
+ *   async someMethod() {
+ *     console.log('Método original executado');
+ *   }
+ *
+ *   async paintCallback() {
+ *     console.log('Callback de pintura chamado');
+ *   }
+ * }
  */
 const repaint = (_target, _propertyKey, descriptor) => {
   // Obtém o valor original do método ou define uma função padrão vazia
--- src/dom/repaint/types.d.ts
+++ src/dom/repaint/types.d.ts
@@ -1,10 +1,26 @@
 /**
- * Decorator utilizado para forçar a atualização da renderização do elemento customizado.
+ * Decorator para chamar o callback de pintura após a execução do método original.
  *
- * @param {any} target - A classe alvo onde o método decorado está sendo definido.
- * @param {PropertyKey} propertyKey - O nome do método decorado.
- * @param {PropertyDescriptor} descriptor - O descritor da propriedade que define o método decorado.
+ * O decorator garante que o callback `paintCallback` seja chamado após a execução do método original,
+ * caso o elemento esteja conectado ao DOM. É útil para garantir que ações de pintura sejam feitas
+ * no momento certo do ciclo de vida do componente.
+ *
+ * @param {Object} _target - O alvo do decorator (classe ou protótipo).
+ * @param {string} _propertyKey - O nome da propriedade/método decorado.
+ * @param {Object} descriptor - O descritor de propriedade/método.
  * @returns {void}
+ *
+ * @example
+ * class MyComponent extends HTMLElement {
+ *   @repaint
+ *   async someMethod() {
+ *     console.log('Método original executado');
+ *   }
+ *
+ *   async paintCallback() {
+ *     console.log('Callback de pintura chamado');
+ *   }
+ * }
  */
 export declare function repaint(
   target: any,
