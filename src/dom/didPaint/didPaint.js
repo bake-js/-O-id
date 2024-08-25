59547716a10942e56d689605b75252502211b521
deMGoncalves <cleber.engineer@hotmail.com>
2024-08-25 16:13:55 -0300

    docs: padroniza a documentacao do modulo did paint

--- src/dom/didPaint/didPaint.js
+++ src/dom/didPaint/didPaint.js
@@ -2,11 +2,21 @@ import intercept, { exec } from "../intercept";
 import { didPaintCallback } from "../interfaces";
 
 /**
- * Cria um decorator para adicionar lógica ao método `didPaintCallback` de um Custom Element.
+ * Cria um decorator que intercepta e adiciona lógica ao método `didPaintCallback` de um Custom Element.
  *
- * @param target - O alvo do decorator, geralmente a classe do Custom Element.
- * @param propertyKey - O nome do método decorado.
- * @returns Um decorator que intercepta a chamada do `didPaintCallback`.
+ * @function
+ * @param {Object} target - O alvo do decorator, geralmente a classe do Custom Element.
+ * @param {string} propertyKey - O nome do método decorado.
+ * @returns {void} Um decorator que intercepta a chamada do `didPaintCallback`.
+ * @description O decorator `didPaint` permite que desenvolvedores de Custom Elements adicionem lógica adicional ao ciclo de vida do componente, especificamente após a renderização.
+ * @example
+ * // Exemplo de uso do decorator didPaint
+ * class MyComponent extends HTMLElement {
+ *   @didPaint
+ *   didPaintCallback() {
+ *     console.log('O componente foi pintado!');
+ *   }
+ * }
  */
 const didPaint = (target, propertyKey) => {
   // Cria uma instância do interceptor para o método `didPaintCallback`.
--- src/dom/didPaint/types.d.ts
+++ src/dom/didPaint/types.d.ts
@@ -1,8 +1,19 @@
 /**
  * Decorator utilizado para definir um método como manipulador de pintura.
- * O método decorado será chamado antes do método decorado com `@paint`.
+ * O método decorado será chamado após o método decorado com `@paint`.
  *
- * @param {any} target - O alvo do decorator.
- * @param {PropertyKey} propertyKey - A chave da propriedade.
+ * @function
+ * @param {any} target - O alvo do decorator, geralmente a classe ou protótipo do Custom Element.
+ * @param {PropertyKey} propertyKey - A chave da propriedade, normalmente o nome do método.
+ * @returns {void}
+ * @description O decorator `didPaint` permite que métodos específicos sejam executados como parte do ciclo de vida de um Custom Element, logo após a renderização.
+ * @example
+ * // Exemplo de uso do decorator didPaint
+ * class MyComponent extends HTMLElement {
+ *   @didPaint
+ *   didPaintCallback() {
+ *     console.log('O componente foi pintado!');
+ *   }
+ * }
  */
 export declare function didPaint(target: any, propertyKey: PropertyKey): void;
