0f56e55679379775a606fc16751c04ccce9d3d4f
deMGoncalves <cleber.engineer@hotmail.com>
2024-08-25 16:08:51 -0300

    docs: padroniza a doc do css

--- src/dom/css/css.js
+++ src/dom/css/css.js
@@ -1,9 +1,19 @@
 /**
  * Cria uma folha de estilos CSS a partir de template literals.
  *
+ * @function
  * @param {TemplateStringsArray} strings - As partes literais da string do template.
  * @param {...any} values - Os valores interpolados na string do template.
  * @returns {CSSStyleSheet[]} Um array contendo a folha de estilos gerada.
+ * @description Esta função permite a criação dinâmica de folhas de estilo CSS utilizando template literals, permitindo a interpolação de valores dentro da string CSS. Ela retorna a folha de estilo gerada como um array de `CSSStyleSheet`.
+ * @example
+ * // Exemplo de uso da função css
+ * const styles = css`
+ *   body {
+ *     background-color: ${backgroundColor};
+ *   }
+ * `;
+ * document.adoptedStyleSheets = [...document.adoptedStyleSheets, ...styles];
  */
 const css = (strings, ...values) => {
   // Cria uma nova instância de CSSStyleSheet.
--- src/dom/css/types.d.ts
+++ src/dom/css/types.d.ts
@@ -1,10 +1,23 @@
 /**
- * Helper para criar uma instância de CSSStyleSheet a partir de um template literal.
+ * Cria uma instância de CSSStyleSheet a partir de um template literal.
  * Facilita a definição de estilos encapsulados para Web Components.
  *
+ * @function
  * @param {TemplateStringsArray} strings - As partes literais do template string.
  * @param {...any} values - Os valores interpolados no template string.
- * @returns {CSSStyleSheet} - A instância de CSSStyleSheet criada a partir do template literal.
+ * @returns {CSSStyleSheet} A instância de CSSStyleSheet criada a partir do template literal.
+ * @description Este helper simplifica a criação de folhas de estilo encapsuladas usando template literals, ideal para uso em Web Components.
+ * @example
+ * // Exemplo de uso do helper css
+ * const styles = css`
+ *   :host {
+ *     display: block;
+ *   }
+ *   .button {
+ *     color: ${buttonColor};
+ *   }
+ * `;
+ * shadowRoot.adoptedStyleSheets = [styles];
  */
 export declare function css(
   strings: TemplateStringsArray,
