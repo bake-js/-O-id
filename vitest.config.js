514d3ff87ded8c64683de1fb1422c2d8468f1c0a
deMGoncalves <cleber.engineer@hotmail.com>
2024-09-09 10:29:08 -0300

    test: troca do bun test pelo vitest

--- vitest.config.js
+++ vitest.config.js
@@ -5,6 +5,12 @@ export default defineConfig({
   test: {
     coverage: {
       reporter: [["lcov", { projectRoot: "./src" }]],
+      thresholds: {
+        statements: 56,
+        branches: 46,
+        functions: 36,
+        lines: 56,
+      },
     },
     environment: "jsdom",
     setupFiles: [resolve(__dirname, "happydom.js")],
