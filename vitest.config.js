7b35d0e9affaa00b751ecb3932bfa3fc50e821ad
deMGoncalves <cleber.engineer@hotmail.com>
2024-09-10 10:08:58 -0300

    ci: configura os arquvivos que devem ser ignorados pelo sonar

--- vitest.config.js
+++ vitest.config.js
@@ -4,19 +4,19 @@ import { defineConfig } from "vite";
 export default defineConfig({
   test: {
     coverage: {
-      include: ["src/**/*.js"],
+      include: ["src/**/*.{js,ts}"],
       exclude: [
-        "src/**/*.spec.ts",
+        "src/**/*.test.{js,ts}",
         "src/**/index.js",
         "src/**/types.d.ts",
         "src/**/README.md",
       ],
       reporter: ["lcov", "text"],
       thresholds: {
-        statements: 60,
-        branches: 79,
+        statements: 57,
+        branches: 74,
         functions: 66,
-        lines: 60,
+        lines: 57,
       },
     },
     environment: "jsdom",
