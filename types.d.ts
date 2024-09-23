6c989e096fe8f44329af7c639920bc4512e24c59
deMGoncalves <cleber.engineer@hotmail.com>
2024-09-23 11:14:43 -0300

    docs: tipagem e jsdoc para o filter prevent

--- types.d.ts
+++ types.d.ts
@@ -608,3 +608,24 @@ export declare const on: {
 export declare const formData: (
   event: Event,
 ) => Record<string, FormDataEntryValue>;
+
+/**
+ * Impede o comportamento padrão de um evento.
+ *
+ * @param {Event} event - O evento a ser filtrado.
+ * @returns {Event} O próprio evento, após impedir o comportamento padrão.
+ *
+ * @description
+ * Este filtro chama o método `preventDefault` do evento, evitando que a ação padrão associada ao evento
+ * seja executada. Após impedir o comportamento padrão, o próprio evento é retornado, permitindo
+ * que outras operações sejam realizadas com o evento modificado.
+ *
+ * @example
+ * import prevent from '@bake-js/-o-id/event';
+ *
+ * function handleClick(event: Event) {
+ *   prevent(event);
+ *   return event;
+ * }
+ */
+export declare const prevent: (event: Event) => Event;
