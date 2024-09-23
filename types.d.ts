36220123a62520f3b49282d03b1084cc401043e0
deMGoncalves <cleber.engineer@hotmail.com>
2024-09-23 10:07:45 -0300

    docs: tipagem e jsdoc para o mixin echo

--- types.d.ts
+++ types.d.ts
@@ -504,3 +504,27 @@ export declare function repaint(
  * }
  */
 export declare function willPaint(target: any, propertyKey: PropertyKey): void;
+
+/**
+ * Mixin Echo para adicionar suporte a um Event Bus em um Custom Element.
+ *
+ * @param {typeof HTMLElement} Klass - A classe do Custom Element a ser estendida.
+ * @returns {typeof HTMLElement} A classe estendida com suporte ao Event Bus.
+ *
+ * @description
+ * Este mixin permite que um Custom Element participe de um Event Bus, escutando e emitindo eventos
+ * de maneira centralizada. Atributos específicos, como `on`, são usados para definir as regras
+ * de comunicação entre componentes.
+ *
+ * @example
+ * import Echo from '@bake-js/-o-id/echo';
+ *
+ * class MyComponent extends Echo(HTMLElement) {
+ *   connectedCallback() {
+ *     console.log('MyComponent conectado');
+ *   }
+ * }
+ *
+ * customElements.define('my-component', MyComponent);
+ */
+export declare function Echo<T extends typeof HTMLElement>(Klass: T): T;
