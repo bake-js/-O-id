9a98f6b40bb49be5ee34a2aabbb3501e549932a0
deMGoncalves <cleber.engineer@hotmail.com>
2024-09-23 09:33:11 -0300

    docs: documentar o types para attributeChanged

--- types.d.ts
+++ types.d.ts
@@ -26,14 +26,33 @@
 export declare function adopted(target: any, propertyKey: PropertyKey): void;
 
 /**
- * Decorator utilizado para definir um método como manipulador de atributos.
- * O método decorado será chamado sempre que o atributo especificado for alterado.
+ * Decorator que adiciona lógica ao método `attributeChangedCallback` de um Custom Element.
+ * Permite que um método seja executado quando um atributo específico é alterado.
  *
- * @param {string} attributeName - O nome do atributo a ser observado.
+ * @param {string} attributeName - O nome do atributo a ser monitorado.
+ * @returns {Function} Um decorator que intercepta a chamada do `attributeChangedCallback`.
+ *
+ * @description
+ * O decorator `attributeChanged` é utilizado para adicionar lógica ao método `attributeChangedCallback`
+ * de um Custom Element. Quando um atributo específico (definido por `attributeName`) é alterado, o método
+ * decorado é chamado com os valores antigo e novo do atributo. O decorator também garante que o atributo
+ * seja adicionado à lista de atributos observados do Custom Element.
+ *
+ * @example
+ * import { attributeChanged } from '@bake-js/-o-id';
+ *
+ * class MyElement extends HTMLElement {
+ *   @attributeChanged('my-attribute')
+ *   handleAttributeChange(newValue, oldValue) {
+ *     console.log(`Atributo alterado de ${oldValue} para ${newValue}`);
+ *   }
+ * }
+ *
+ * customElements.define('my-element', MyElement);
  */
 export declare function attributeChanged(
   attributeName: string,
-): (target: any, propertKey: PropertyKey) => void;
+): (target: any, propertyKey: PropertyKey) => void;
 
 /**
  * Decorator utilizado para modificar o comportamento do método `connectedCallback` de um elemento customizado.
