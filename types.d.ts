fe68adbbe46403d3089218cc845e8b3b8d6de636
deMGoncalves <cleber.engineer@hotmail.com>
2024-09-23 11:20:04 -0300

    docs: tipagem e jsdoc para o modulo stop

--- types.d.ts
+++ types.d.ts
@@ -629,3 +629,25 @@ export declare const formData: (
  * }
  */
 export declare const prevent: (event: Event) => Event;
+
+/**
+ * Interrompe a propagação de um evento.
+ *
+ * @param {Event} event - O evento a ser filtrado.
+ * @returns {Event} O próprio evento, após interromper a propagação.
+ *
+ * @description
+ * Este filtro chama o método `stopPropagation` do evento, evitando que o evento continue a ser propagado para
+ * outros elementos no DOM. Após interromper a propagação, o próprio evento é retornado, permitindo
+ * que outras operações sejam realizadas com o evento modificado.
+ *
+ * @example
+ * import { stop } from '@bake-js/-o-id/event';
+ *
+ * // Exemplo de uso em um manipulador de evento:
+ * function handleClick(event: Event) {
+ *   stop(event);
+ *   return event;
+ * }
+ */
+export declare const stop: (event: Event) => Event;
