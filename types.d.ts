e19e6eb397c8423b4c4fd11d10a2b818f2f2d567
deMGoncalves <cleber.engineer@hotmail.com>
2024-09-23 09:41:20 -0300

    docs: tipagem e jsdoc para o disconnected

--- types.d.ts
+++ types.d.ts
@@ -108,3 +108,37 @@ export declare function define(
   name: string,
   options?: ElementDefinitionOptions,
 ): (constructor: CustomElementConstructor) => void;
+
+/**
+ * Decorator que adiciona lógica ao método `disconnectedCallback` de um Custom Element.
+ *
+ * @param {Object} target - O alvo do decorator, geralmente a classe do Custom Element.
+ * @param {string} propertyKey - O nome do método decorado.
+ * @returns {Function} Um decorator que intercepta a chamada do `disconnectedCallback`.
+ *
+ * @description
+ * O decorator `disconnected` adiciona lógica ao método `disconnectedCallback` de um Custom Element. Ele permite
+ * que você execute uma função personalizada quando o elemento é removido do DOM. A função decorada será chamada
+ * automaticamente quando o `disconnectedCallback` for invocado.
+ *
+ * @example
+ * import { disconnected } from '@bake-js/-o-id';
+ *
+ * class MyElement extends HTMLElement {
+ *   constructor() {
+ *     super();
+ *     this.attachShadow({ mode: 'open' });
+ *   }
+ *
+ *   @disconnected
+ *   handleDisconnect() {
+ *     console.log('Elemento foi removido do DOM.');
+ *   }
+ * }
+ *
+ * customElements.define('my-element', MyElement);
+ */
+export declare function disconnected(
+  target: any,
+  propertyKey: PropertyKey,
+): void;
