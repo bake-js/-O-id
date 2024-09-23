4f9791168a7b01ac97580b923c33683c7be3b350
deMGoncalves <cleber.engineer@hotmail.com>
2024-09-23 09:49:30 -0300

    docs: inclusao da tipagem e jsdoc do didPaint

--- types.d.ts
+++ types.d.ts
@@ -288,6 +288,8 @@ export declare function formStateRestore(
  * retorna a folha de estilo gerada como um array de `CSSStyleSheet`.
  *
  * @example
+ * import { css } from '@bake-js/-o-id/dom';
+ *
  * const styles = css`
  *   body {
  *     background-color: ${backgroundColor};
@@ -300,3 +302,26 @@ export declare function css(
   strings: TemplateStringsArray,
   ...values: any[]
 ): CSSStyleSheet;
+
+/**
+ * Cria um decorator que intercepta e adiciona lógica ao método `didPaintCallback` de um Custom Element.
+ *
+ * @param {Object} target - O alvo do decorator, geralmente a classe do Custom Element.
+ * @param {string} propertyKey - O nome do método decorado.
+ * @returns {void} Um decorator que intercepta a chamada do `didPaintCallback`.
+ *
+ * @description
+ * O decorator `didPaint` permite que desenvolvedores de Custom Elements adicionem
+ * lógica adicional ao ciclo de vida do componente, especificamente após a renderização.
+ *
+ * @example
+ * import { didPaint } from '@bake-js/-o-id/dom';
+ *
+ * class MyComponent extends HTMLElement {
+ *   @didPaint
+ *   handleDidPaint() {
+ *     console.log('O componente foi pintado!');
+ *   }
+ * }
+ */
+export declare function didPaint(target: any, propertyKey: PropertyKey): void;
