8a4728a2ba635995c7d31ed9471e65328d958532
deMGoncalves <cleber.engineer@hotmail.com>
2024-09-23 09:46:08 -0300

    docs: inclusao da tipagem e jsdoc para o css

--- types.d.ts
+++ types.d.ts
@@ -275,3 +275,28 @@ export declare function formStateRestore(
   target: any,
   propertyKey: PropertyKey,
 ): void;
+
+/**
+ * Cria uma folha de estilos CSS a partir de template literals.
+ *
+ * @param {TemplateStringsArray} strings - As partes literais da string do template.
+ * @param {...any} values - Os valores interpolados na string do template.
+ * @returns {CSSStyleSheet[]} Um array contendo a folha de estilos gerada.
+ *
+ * @description Esta função permite a criação dinâmica de folhas de estilo CSS utilizando
+ * template literals, permitindo a interpolação de valores dentro da string CSS. Ela
+ * retorna a folha de estilo gerada como um array de `CSSStyleSheet`.
+ *
+ * @example
+ * const styles = css`
+ *   body {
+ *     background-color: ${backgroundColor};
+ *   }
+ * `;
+ *
+ * document.adoptedStyleSheets = [...document.adoptedStyleSheets, ...styles];
+ */
+export declare function css(
+  strings: TemplateStringsArray,
+  ...values: any[]
+): CSSStyleSheet;
